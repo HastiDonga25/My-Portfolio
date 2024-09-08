9d6bb46a1dbf04ee3bf73311f71f064cfb74120f
Dharav0126 <dharav2626@gmail.com>
2024-09-08 14:19:04 -0400

    server code initiated

---
+++ src/server.js
@@ -0,0 +1,21 @@
+const express = require("express");
+const path = require("path");
+const dotenv = require ("dotenv");
+
+const app = express();
+
+app.use("/CSS", express.static(path.join(__dirname, "CSS")));
+app.use('/JS', express.static(path.join(__dirname, 'JS')))
+app.use('/IMG', express.static(path.join(__dirname, 'IMG')))
+
+
+app.get('/',(req,res)=>{
+    res.sendFile(path.join(__dirname,'index.html'));
+})
+
+// server start
+const PORT = 2000;
+app.listen(PORT, ()=>{
+    console.log(`server running on http://127.0.0.1:${PORT}`);
+})
+
